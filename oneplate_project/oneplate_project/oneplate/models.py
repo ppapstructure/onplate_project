37df8f428e1976c06b5eeb3b3559669bda0c8b62
JH-S <skskfl5786@gmail.com>
2024-10-04 23:50:51 +0900

    main

---
+++ oneplate_project/oneplate_project/oneplate/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from oneplate.validators import validate_no_special_characters
+
+# Create your models here.
+class User(AbstractUser):
+    nickname = models.CharField(
+        max_length=20,
+        unique=True,
+        null=True,
+        validators=[validate_no_special_characters],
+        error_messages={'unique': '이미 사용중인 닉네임입니다.'},
+    )
+    # profile_pic
+
+    intro = models.CharField(max_length=60, blank=True)
+
+    class Meta:
+        db_table = 'app_user'
+
+    # def __str__(self):
+    #     return self.email
